db4f66fac918d4766f51cf23ae0c61792348de7b
mgaynor1 <shellygaynor@Shellys-MacBook-Pro.local>
2026-07-10 12:43:27 -0400

    formatting try 4

--- Botany2026/scripts/00_Setup.R
+++ Botany2026/scripts/00_Setup.R
@@ -2,13 +2,13 @@
 #
 # Purpose:
 
-# - Install all R packages required for ENM workflows.
+# Install all R packages required for ENM workflows.
 
-# - Check that all packages load successfully.
+# Check that all packages load successfully.
 
-# - Install any needed GitHub packages.
+# Install any needed GitHub packages.
 
-## Download Zenodo Data
+## Download Zenodo Data ----
 source("scripts/tools/ZenodoDownload.R")
 download.packages(c("tools", "httr2", "utils", "magrittr"))
 ZenodoDownload()

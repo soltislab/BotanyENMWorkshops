9b9ec04fb7263b690e48455641657d173d5e8fa9
JT Miller <82229873+jtmiller28@users.noreply.github.com>
2026-07-20 17:41:31 -0400

    Update interactive chunk number for data downloads

--- Botany2026/scripts/tools/webr_config.R
+++ Botany2026/scripts/tools/webr_config.R
@@ -3,7 +3,7 @@
 
 webr_interactive <- list(
   "00_Setup" = c(),  # No interactive chunks yet
-  "01_Data_Downloads" = c(3), # Example: chunk 3 is interactive
+  "01_Data_Downloads" = c(5), # Example: chunk 3 is interactive
   "02_Data_Cleaning" = c(),
   "03_Data_Exploration" = c(),  
   "04_AccessibleArea_VariableSelection" = c(),
